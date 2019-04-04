69f43379f62e5d899e1146e6ec4f50a7c4acb4d6
Anirudh <anirudh@pop-os.localdomain>
2019-04-04 13:12:04 +0530

    imlemented singly linked list for deque

---
+++ queues/Deque.java
@@ -0,0 +1,97 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Deque<Item> implements Iterable<Item> {
+    private Node<Item> first;
+    private Node<Item> last;
+    private int n;
+
+    private static class Node<Item> {
+        private Item item;
+        private Node<Item> next;
+        private Node<Item> prev;
+    }
+
+    public Deque() {
+        first = null;
+        last = null;
+        n = 0;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public void addFirst(Item item) {
+
+    }
+
+    public void addLast(Item item) {
+        Node<Item> oldlast = last;
+        last = new Node<Item>();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()) first = last;
+        else oldlast.next = last;
+        n++;
+
+    }
+
+    public Item removeFirst() {
+        if (isEmpty()) throw new NoSuchElementException("Queue Underflow!");
+        Item item = first.item;
+        first = first.next;
+        n--;
+        if (isEmpty()) last = null;
+        return item;
+
+    }
+
+    public Item removeLast() {
+        return first.item;
+
+    }
+
+    public Iterator<Item> iterator() {
+        return new ListIterator(first);
+    }
+
+    private class ListIterator implements Iterator<Item> {
+        private Node<Item> current;
+
+        public ListIterator(Node<Item> first) {
+            current = first;
+        }
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+
+    public static void main(String[] args) {
+        Deque<Integer> d = new Deque<>();
+        d.addLast(2);
+        d.addLast(3);
+        for (int s : d) {
+            System.out.println(s);
+        }
+        System.out.println(d.removeFirst());
+    }
+
+
+}
